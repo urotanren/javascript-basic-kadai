99d8ba39835f860b5d9403b0636d54c8a8472d05
urotanren <148876227+urotanren@users.noreply.github.com>
2023-11-04 00:26:50 +0900

    kadai_007再提出をアップロードしました

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -1,12 +1,13 @@
-let num = 300;
-if (num % 3 == 0){
-    console.log("3の倍数です");
-}
-if(num % 5 == 0){
-    console.log("5の倍数です");
-}
+let num = 16;
+
 if(num % 3 == 0 && num % 5 == 0){
     console.log ("3と5の倍数です");
+}
+else if (num % 3 == 0){
+    console.log ("3の倍数です");
+}
+else if (num % 5 == 0){
+    console.log ("5の倍数です");
 }
 else{
     console.log(num);
