36a6633acb942b14c8fd3d3e150de8023a53bbc1
urotanren <148876227+urotanren@users.noreply.github.com>
2023-11-06 13:29:29 +0900

    再提出kadai_014をアップデートしました。

---
+++ kadai_014/square.js
@@ -0,0 +1,15 @@
+const squared = (n) => {
+    // console.log(n * n);
+    return n * n;
+};
+
+// squared(10);
+// squared(5);
+// squared(2);
+
+console.log(squared(10));
+console.log(squared(5));
+console.log(squared(2));
+
+
+
